4ac3e45f1b13f8083b7a1574e3ae621d0bbcf1e8
Tamar Mirbach <tamarmirbach@gmail.com>
2021-07-06 17:46:48 +0200

    extract apply blocked change

---
+++ src/application/service/ApplyBlockedChange.java
@@ -0,0 +1,23 @@
+package application.service;
+
+import java.util.ArrayList;
+
+public class ApplyBlockedChange {
+	/*
+	 * add blocked entries
+	 */
+	public static Street addBlocked(Str8tSolver str8t, Street s, ArrayList<Integer> newBlocked) {
+		ArrayList<Integer> blocked = s.getBlocked();
+		// remove duplicates
+		blocked = Helper.deleteDuplicates(blocked);
+		blocked.addAll(newBlocked);
+		s.setBlocked(Helper.deleteDuplicates(blocked));
+		// those are not longer (only) possible
+		for (int i = 0; i < newBlocked.size(); i++) {
+			int m = newBlocked.get(i);
+			s = ApplyPossibleChange.removePossible(str8t, s,m);
+		}
+		return s;
+	}
+	
+}
